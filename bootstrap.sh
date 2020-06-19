2318345e79b1ee1bf2624796c680442d4d448827
Sam Macbeth <sam@macbeth.cc>
2020-06-19 16:37:43 +0200

    Update extension link

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-TAG=v8.5.1
+TAG=v8.5.2
 EXT_DIR=app/src/main/assets/extensions/ghostery
+DOWNLOAD_URL=https://cdncliqz.s3.amazonaws.com/update/fostery/ghostery-firefox-$TAG.zip
 mkdir -p $EXT_DIR
-curl -L -o ghostery.zip https://github.com/ghostery/ghostery-extension/releases/download/$TAG/ghostery-firefox-$TAG.zip
+curl -L -o ghostery.zip $DOWNLOAD_URL
 unzip ghostery.zip -d $EXT_DIR
 rm ghostery.zip
