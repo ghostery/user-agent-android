60e40438f8aeeb8ed2d4c06c9eb78818d932bbbd
Sam Macbeth <sammacbeth@users.noreply.github.com>
2020-07-10 11:22:59 +0200

    Bump ghostery extension version (#12)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG=v8.5.2
+TAG=v8.5.3
 EXT_DIR=app/src/main/assets/extensions/ghostery
 DOWNLOAD_URL=https://cdncliqz.s3.amazonaws.com/update/fostery/ghostery-firefox-$TAG.zip
 mkdir -p $EXT_DIR
