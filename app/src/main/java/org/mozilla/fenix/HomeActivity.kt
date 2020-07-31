14d02a9f2de6b3910180e29fefa9d38ccdacac58
Stefano Pacifici <stefano.pacifici@gmail.com>
2020-07-31 11:05:48 +0200

    Replace the theme before creating views

--- app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -145,6 +145,10 @@ open class HomeActivity : LocaleAwareAppCompatActivity() {
 
         components.publicSuffixList.prefetch()
 
+        /* Ghostery Begin: Splashscreen, restore the regular theme before creating the views */
+        setTheme(R.style.NormalTheme)
+        /* Ghostery End */
+
         setupThemeAndBrowsingMode(getModeFromIntentOrLastKnown(intent))
         setContentView(R.layout.activity_home)
 
