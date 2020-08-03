d2a541155d0ce3803f7de7d10b9c34c9e9f538f8
Sam Macbeth <sam@cliqz.com>
2020-08-03 18:31:14 +0200

    Basic ghostery extension integration

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TAG=v8.5.2
+EXT_DIR=app/src/main/assets/extensions/ghostery
+DOWNLOAD_URL=https://cdncliqz.s3.amazonaws.com/update/fostery/ghostery-firefox-$TAG.zip
+mkdir -p $EXT_DIR
+curl -L -o ghostery.zip $DOWNLOAD_URL
+unzip ghostery.zip -d $EXT_DIR
+rm ghostery.zip
