ed7409389b4eff2a1b283d7f7522651d02091e09
Sam Macbeth <sam@cliqz.com>
2020-09-19 19:38:20 +0200

    Cleanup patches

--- import.sh
+++ import.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
+
 # apply patches
-(cd browser; git am --ignore-space-change --ignore-whitespace ../patches/*)
+(cd browser && git am --ignore-space-change --ignore-whitespace ../patches/*)
 # copy assets into browser tree
 rsync -av ./assets/ ./browser/
