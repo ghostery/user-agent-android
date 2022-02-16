d719c8a91b30889790e2f8c431ffac69149a0172
Krzysztof Modras <chrmod@chrmod.net>
2022-02-16 17:00:09 +0100

    Update Ghostery Extension Version

--- assets/bootstrap.sh
+++ assets/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-TAG="v8.5.2-4"
+set -x
+set -e
 EXT_DIR="app/src/main/assets/extensions/ghostery"
-DOWNLOAD_URL="https://cdncliqz.s3.amazonaws.com/update/fostery/ghostery-firefox-$TAG.zip"
+DOWNLOAD_URL="https://github.com/ghostery/ghostery-extension/releases/download/v8.6.2/ghostery-dawn-v8.6.2.zip"
 mkdir -p "$EXT_DIR"
 curl -L -o ghostery.zip "$DOWNLOAD_URL"
 unzip -o ghostery.zip -d "$EXT_DIR"
